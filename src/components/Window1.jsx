01aee1cd457f24ef54227aa6db43d81dc5d1b223
Shan2411 <shandavid2411@gmail.com>
2025-07-24 13:54:29 +0800

    overbloated window component from hell i used a bit of ai for that rect variable part

--- src/components/Window1.jsx
+++ src/components/Window1.jsx
@@ -1,42 +1,47 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect, useRef} from "react";
 import "./Window1.css"
 
 function Window1 () {
 
-
+    const panel = useRef(null)
     const [coords, setcoords] = useState({x: 0, y: 0});
-    const [newcoords, setnewcoords] = useState ({newX: 0, newY: 0});
+    const [newcoords, setnewcoords] = useState({ newX: 0, newY: 0 });
+
+    const mouseU = (event) => {
+      document.removeEventListener("mousemove", mouseM);
+    };
+    const mouseM = (event) => {
+      setnewcoords({ newX: event.clientX, newY: event.clientY });
+    };
+    const mouseD = (event) => {
+        const rect = panel.current.getBoundingClientRect();
+
+        setnewcoords({ newX: event.clientX, newY: event.clientY });
+        setcoords({ x: event.clientX - rect.left, y: event.clientY - rect.top });
+
+        document.addEventListener("mousemove", mouseM);
+        document.addEventListener("mouseup", mouseU);
+    };
 
     useEffect(() => {
+      const logs = (event) => {
+        console.log(newcoords.newX);
+      };
 
-        const mouseU = (event) => {
-            panel.removeEventListener('mousemove', mouseM);
-        }
-        const mouseM = (event) => {
-            setnewcoords({newX: event.clientX, newY: event.clientY});
-            
-        }
-        const mouseD = (event) => {
-            setcoords({x: event.clientX, y: event.clientY});
-            
-            panel.addEventListener('mousemove', mouseM)
-            panel.addEventListener('mouseup', mouseU)
-        }
-        const logs = (event) => {console.log(newcoords.newX)}
-
-        const panel = document.querySelector(".windowCont")
-        
-        panel.addEventListener('mousedown', mouseD)
-        document.addEventListener('keydown', logs)
+      panel.current.addEventListener("mousedown", mouseD);
+      document.addEventListener("keydown", logs);
 
       return () => {
-        
-      }
-    });
+        document.removeEventListener("keydown", logs);
+        document.removeEventListener("mousemove", mouseM);
+        document.removeEventListener("mouseup", mouseU);
+        //panel.current.removeEventListener("mousedown", mouseD)
+      };
+    }, []);
 
     return(
 
-        <div className="windowCont" style={{top: newcoords.newY - coords.y, left: newcoords.newX - coords.x}}>
+        <div ref={panel} className="windowCont" style={{top: newcoords.newY - coords.y, left: newcoords.newX - coords.x}}>
             <div className="header">
 
             </div>
